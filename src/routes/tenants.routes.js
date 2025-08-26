d526fc59db4de106ba85a6d29c1172b11924b9bb
nikeshadhikari9 <nickeshadhikari@gmail.com>
2025-08-26 21:17:41 +0545

    removed unuseful tenants routes

--- src/routes/tenants.routes.js
+++ src/routes/tenants.routes.js
@@ -1,9 +1,4 @@
 const express = require("express");
 const router = express.Router();
 
-const { updateUserLocation } = require("../controllers/tenants.controller.js")
-
-router.post('/login', loginAdmin);
-router.post('/logout', logout)
-
 module.exports = router;
