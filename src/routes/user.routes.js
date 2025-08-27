f9f281c970a4bb0b66ed8bca90a81b2aaca59ddb
nikeshadhikari9 <nickeshadhikari@gmail.com>
2025-08-27 13:10:05 +0545

    added user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const upload = require("../middlewares/multer.middleware.js");
+
 const { updateUserLocation } = require("../controllers/tenants.controller.js")
 const { verifyLoggedInUser } = require("../middlewares/auth.middleware.js")
+const { getUserDetails, updateUser } = require("../controllers/user.controller.js");
 
 router.route('/update-location/:lat/:lng').post(verifyLoggedInUser, updateUserLocation);
+router.route('/:id').get(verifyLoggedInUser, getUserDetails);
+router.route('/update-user').patch(verifyLoggedInUser, upload.single("profileImage"), updateUser);
 
 
 module.exports = router;
