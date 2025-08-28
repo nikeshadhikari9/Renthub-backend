fcab405c72e90faf98d419ce8763b5d76f5351a3
nikeshadhikari9 <nickeshadhikari@gmail.com>
2025-08-28 23:18:36 +0545

    added a new landlord route

--- src/routes/landlord.routes.js
+++ src/routes/landlord.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { updateUserLocation } = require("../controllers/tenants.controller.js")
+const { roomsByLandlord } = require("../controllers/landlord.controller.js")
+const { verifyLandlord } = require("../middlewares/auth.middleware.js")
+
+router.route('/my-rooms').get()
 
 
 module.exports = router;
