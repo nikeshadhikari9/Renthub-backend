c9c512fde447b353ce846512f25583d62241edf9
nikeshadhikari9 <nickeshadhikari@gmail.com>
2025-08-28 22:09:27 +0545

    added esewa related env variables

--- src/config/env.js
+++ src/config/env.js
@@ -15,4 +15,10 @@ module.exports = {
   API_SECRET: process.env.API_SECRET,
   SIGHT_API_SECRET: process.env.SIGHT_API_SECRET,
   SIGHT_API_USER: process.env.SIGHT_API_USER,
+  // esewa env variables
+  MERCHANT_ID: process.env.MERCHANT_ID,
+  MERCHANT_SECRET: process.env.MERCHANT_SECRET,
+  SUCCESS_URL: process.env.SUCCESS_URL,
+  FAILURE_URL: process.env.FAILURE_URL,
+  ESEWAPAYMENT_URL: process.env.ESEWAPAYMENT_URL
 }
