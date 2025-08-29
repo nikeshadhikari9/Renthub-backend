0ceff7a643adbf0e859cf44d4f6f2fec5a056fc4
nikeshadhikari9 <nickeshadhikari@gmail.com>
2025-08-29 22:10:33 +0545

    updated  transaction route

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { verifyLoggedInUser } = require("../middlewares/auth.middleware.js")
 const { paymentStatus } = require("../controllers/transaction.controller.js")
 
 // route to verify payment of esewa
-router.route('/esewa/payment-status').post(verifyLoggedInUser, paymentStatus);
+router.route('/esewa/payment-status').post(paymentStatus);
 
 module.exports = router;
