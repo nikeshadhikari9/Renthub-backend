5e5003693a4cff0151b59c4c43106634f991ed54
nikeshadhikari9 <nickeshadhikari@gmail.com>
2025-08-29 22:14:01 +0545

    added amiddleware for authorization

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const { verifyLoggedInUser } = require("../middlewares/auth.middleware.js")
 const { paymentStatus } = require("../controllers/transaction.controller.js")
 
 // route to verify payment of esewa
-router.route('/esewa/payment-status').post(paymentStatus);
+router.route('/esewa/payment-status').post(verifyLoggedInUser, paymentStatus);
 
 module.exports = router;
